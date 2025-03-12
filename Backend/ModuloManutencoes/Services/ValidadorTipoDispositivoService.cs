e26de5233af083fd2e2947ead48ff2470ac9afb7
Pedro Perez <vidadeenderman@gmail.com>
2025-03-11 23:17:19 -0300

    feat: Aplicando 'S' do SOLID nas classes da API parte 2

---
+++ Backend/ModuloManutencoes/Services/ValidadorTipoDispositivoService.cs
@@ -0,0 +1,25 @@
+﻿
+using ModuloManutencoes.Exceptions.TipoDispositivoExceptions;
+using ModuloManutencoes.Repositories.Interfaces;
+using ModuloManutencoes.Services.Interfaces;
+
+namespace ModuloManutencoes.Services
+{
+    public class ValidadorTipoDispositivoService : IValidadorTipoDispositivoService
+    {
+        private readonly ITipoDispositivoRepository _repository;
+
+        public ValidadorTipoDispositivoService(ITipoDispositivoRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task ValidarTipoDispositivo(int id)
+        {
+            if (!await _repository.ValidarSeTipoDispositivoExiste(id))
+            {
+                throw new TipoDispositivoNaoEncontradoException();
+            }
+        }
+    }
+}
